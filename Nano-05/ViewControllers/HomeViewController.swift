d39115612c3d8079c940e6e3670a38a649b86852
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-02-28 11:36:51 -0300

    Adiciona conteúdo da tableView + posts referentes à home

--- Nano-05/ViewControllers/HomeViewController.swift
+++ Nano-05/ViewControllers/HomeViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate {
 
     @IBOutlet weak var saibaMaisButton: UIButton!
     @IBOutlet weak var lineChartView: UIView!
     
     @IBOutlet weak var whoTreatmentDataView: UIView!
     @IBOutlet weak var whoInfectedDataView: UIView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // parte do rogério
+    // falta tabID
+    private var homeTab: Tab!
+    let homeTabId = "1"
+    private var tableViewHeight: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +31,86 @@ class HomeViewController: UIViewController {
         lineChartView.layer.cornerRadius = 10
         whoTreatmentDataView.layer.cornerRadius = 10
         whoInfectedDataView.layer.cornerRadius = 10
+        
+        // Tab Item
+        let auxTab: [Tab] = tab.filter { $0.id == 1}
+        guard let homeTab = auxTab.first else { return }
+        self.homeTab = homeTab
+        
+        // MARK: Table View
+        let px = 1 / UIScreen.main.scale
+        let frame = CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: px)
+        
+        let line = UIView(frame: frame)
+        
+        self.tableView.tableHeaderView = line
+        line.backgroundColor = self.tableView.separatorColor
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isScrollEnabled = false
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+
+    }
+    
+    //MARK: ViewDidLayoutSubviews
+    override func viewDidLayoutSubviews() {
+        tableView.reloadData()
+        tableView.layoutIfNeeded()
+
+        if let heightContraint = tableViewHeight {
+            heightContraint.constant = tableView.contentSize.height
+        } else {
+            tableViewHeight = tableView.heightAnchor.constraint(equalToConstant: tableView.contentSize.height)
+            tableViewHeight?.isActive = true
+        }
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        if let row = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: row, animated: true)
+        }
+    }
+
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        cell.textLabel?.text = homeTab.listItens[indexPath.row].title
+
+        cell.isAccessibilityElement = true
+
+        if let hint = homeTab.listItens[indexPath.row].titleHint {
+            cell.accessibilityHint = hint
+        } else {
+            cell.accessibilityHint = "Clique para ver mais sobre \(homeTab.listItens[indexPath.row].title)"
+        }
+
+        cell.accessibilityValue = "Botão \(indexPath.row) de \(homeTab.listItens.count)"
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let value = homeTabId + "\(indexPath.row)"
+        let loadVC = PostViewController()
+
+        guard let post = postById(Int(value)!) else {
+            fatalError("Impossível redirecionar. Post não encontrado")
+        }
+
+        loadVC.setPost(post)
+
+        loadVC.modalPresentationStyle = .fullScreen
+        self.navigationController?.pushViewController(loadVC, animated: true)
+    }
 }
