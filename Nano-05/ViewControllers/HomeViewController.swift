ffa9734567de88de1268b70116d40fe8171076a7
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-02-28 00:40:29 -0300

    Adiciona área dos dados da OMS

--- Nano-05/ViewControllers/HomeViewController.swift
+++ Nano-05/ViewControllers/HomeViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var saibaMaisButton: UIButton!
+    @IBOutlet weak var lineChartView: UIView!
+    
+    @IBOutlet weak var whoTreatmentDataView: UIView!
+    @IBOutlet weak var whoInfectedDataView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        saibaMaisButton.layer.cornerRadius = 10
+        lineChartView.layer.cornerRadius = 10
+        whoTreatmentDataView.layer.cornerRadius = 10
+        whoInfectedDataView.layer.cornerRadius = 10
     }
 
 }
